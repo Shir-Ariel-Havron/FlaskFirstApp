c8d968768bcfd699545741adab282b4991021dab
Matan Ariel-Havron <matan.arielhavron1@gmail.com>
2020-03-22 17:40:22 +0200

    Lesson 6: db, models, user handeling

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, ValidationError, Length, Email, EqualTo
+from flaskblog.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -11,6 +12,16 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('confirm_password', validators=[
         DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up!')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username is taken')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email is taken')
 
 
 class LoginForm(FlaskForm):
