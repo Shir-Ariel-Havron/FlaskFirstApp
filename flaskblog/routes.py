eb1e33bfdf2eb2e6d0fccfabb24ab63cd3827e62
Matan Ariel-Havron <matan.arielhavron1@gmail.com>
2020-03-23 18:40:11 +0200

    formatting

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -3,7 +3,8 @@ from flaskblog import app, bcrypt, db
 from flaskblog.models import User, Post
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flask_login import login_user, current_user, logout_user, login_required
-import secrets, os
+import secrets
+import os
 from PIL import Image
 
 
@@ -25,11 +26,14 @@ def register():
         return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_pw)
+        hashed_pw = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        user = User(username=form.username.data,
+                    email=form.email.data, password=hashed_pw)
         db.session.add(user)
         db.session.commit()
-        flash(f'Account created for {form.username.data}! You can log in now!', 'success')
+        flash(
+            f'Account created for {form.username.data}! You can log in now!', 'success')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
@@ -60,8 +64,9 @@ def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, file_ext = os.path.splitext(form_picture.filename)
     picture_file_name = random_hex + file_ext
-    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_file_name)
-    
+    picture_path = os.path.join(
+        app.root_path, 'static/profile_pics', picture_file_name)
+
     output_size = (125, 125)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
@@ -86,7 +91,8 @@ def account():
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.email.data = current_user.email
-    image_file = url_for('static', filename=f'profile_pics/{current_user.image_file}')
+    image_file = url_for(
+        'static', filename=f'profile_pics/{current_user.image_file}')
     return render_template('account.html', title='Account', image_file=image_file, form=form)
 
 
@@ -95,7 +101,8 @@ def account():
 def new_post():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        post = Post(title=form.title.data,
+                    content=form.content.data, author=current_user)
         db.session.add(post)
         db.session.commit()
         flash('Your post has been created!', 'success')
@@ -138,4 +145,3 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('home'))
-    
